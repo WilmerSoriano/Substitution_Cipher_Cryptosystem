bc6597b8ca3dd7f84c329e32cf3b2036743c5ba4
WilmerSoriano <official_Wilmer.2020@outlook.com>
2025-09-02 15:02:59 -0500

    example file and python code has been started

---
+++ SCC.py
@@ -0,0 +1,27 @@
+"""
+Simple Abstraction of code:
+    1st handle the txt file / ask user for file
+    2nd determine the encry/decry
+        - if encrypt
+            - take the message
+            - make a key
+            - hide the message
+        - if decrypt
+            - take the message
+            - use the key
+            - reveal message
+    3rd save the message and print in txt file
+"""
+
+import time
+# 1st. Take user input and handle the file
+
+def handleFile(filename):
+
+if __name__ == '__main__':
+    print("="*24)
+    filename = input("Please enter a filename: ")
+    print("="*24)
+    print("Processing...")
+    #time.sleep(5) just cause it look cooler and legit
+    handleFile(filename)
